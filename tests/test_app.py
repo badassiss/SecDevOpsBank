c8ceff612b73749f17cbeb26e76105b29b7d95c8
Badiss <medbadiss00@gmail.com>
2026-03-09 04:05:34 +0100

    correction du test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 # tests/test_app.py
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
 from app import app
